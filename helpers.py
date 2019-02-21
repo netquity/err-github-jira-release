c7be91d50b30867fa89e35b26cafdd8fa7bd6dd4
Alex Sheluchin <alex@coinaccord.io>
2019-02-21 16:57:08 -0500

    Add bump_version logic and validation

--- helpers.py
+++ helpers.py
@@ -4,6 +4,10 @@ import subprocess
 import logging
 
 
+class InvalidStageTransitionError(Exception):
+    """An invalid stage (seal, sign, send) transition"""
+
+
 def update_changelog_file(
         changelog_path: str,
         release_notes: str,
@@ -33,21 +37,128 @@ def run_subprocess(args: list, cwd: str = None) -> subprocess.CompletedProcess:
     )
 
 
-def bump_version(version: str, release_type: str) -> str:
-    """Perform a version bump in accordance with semver.org."""
-    if release_type == 'Hotfix':  # Don't bump the version number, just append the suffix
-        return str(version) + '-Hotfix'
-    MAJOR = 0
-    MINOR = 1
-    PATCH = 2
+# TODO: add hotfix support
+def bump_version(release_type: str, stage: str, final_version: str, pre_version: str = None) -> str:
+    """Perform a version bump in accordance with semver.org and PEP-440
 
-    # TODO: exceptions
-    release_type = vars()[release_type.upper()]
-    version_array = [int(x) for x in version.split('.', 2)]
-    version_array[release_type] += 1
-    version_array[release_type + 1:] = [0] * (PATCH - release_type)
+    The versioning scheme is a combination of both Semantic Versioning and PEP-440 as described in the README
 
-    return '.'.join(str(version) for version in version_array)
+    :param release_type: the highest release type (of all merged tickets) since the `final_version`
+    :param stage: the release stage to transition into (seal, send, sign)
+    :param final_version: the latest final version name; the version to be upgraded (eg. 1.0.0)
+    :param pre_version: the version containing information about the current release cycle:
+                        pre-release version, rc count, stage (eg. 1.0.1-rc.5+sealed
+                        it's possible that such a version does not yet exist, as when initiating the release sequence
+                        for the first time
+    :return: the new version string
+
+    >>> bump_version('patch', 'seal', '1.0.0',)
+    '1.0.1-rc.1+seal'
+    >>> bump_version('minor', 'seal', '1.0.0',)
+    '1.1.0-rc.1+seal'
+    >>> bump_version('minor', 'send', '1.0.0', '1.1.0-rc.1+seal')
+    '1.1.0-rc.1+send'
+
+    # A full release cycle example
+    >>> bump_version('major', 'seal', '1.0.0',)  # first seal
+    '2.0.0-rc.1+seal'
+    >>> bump_version('major', 'seal', '1.0.0',  '2.0.0-rc.1+seal',)  # second seal
+    '2.0.0-rc.2+seal'
+    >>> bump_version('major', 'send', '1.0.0', '2.0.0-rc.2+seal',)
+    '2.0.0-rc.2+send'
+    >>> bump_version('major', 'sign', '1.0.0', '2.0.0-rc.2+send',)
+    '2.0.0-rc.2+sign'
+    >>> bump_version('major', 'seal', '1.0.0', '2.0.0-rc.2+send',)
+    '2.0.0-rc.3+seal'
+
+    >>> bump_version('major', 'sign', '1.0.0', '2.0.0-rc.2+sign',)
+    Traceback (most recent call last):
+        ...
+    helpers.InvalidStageTransitionError: Cannot transition from sign to sign
+    """
+    import semver
+    if pre_version is not None:
+        pre_stage = pre_version.split('+')[1]
+        if not is_valid_transition(pre_stage, stage):
+            raise InvalidStageTransitionError(f'Cannot transition from {pre_stage} to {stage}')
+    verinfo = semver.parse(pre_version or final_version)
+    bumped_semver = getattr(semver, f'bump_{release_type.lower()}')(final_version)
+
+    if verinfo.get('prerelease', None):
+        bumped_semver += f"-{verinfo['prerelease']}"
+    if stage == 'seal':
+        return semver.bump_prerelease(bumped_semver) + f"+{stage}"
+    return bumped_semver + f"+{stage}"
+
+
+def is_valid_transition(start: str, target: str) -> bool:
+    """Check whether the stage transition is allowed
+
+    The normal order of release stages is: seal, send, sign
+
+    Some transitions are not allowed. For example, a release cannot be signed and then transitioned to sent. This
+    function checks whether the given transition is one of the legal ones.
+
+    :param start: the starting stage
+    :param target: the desired end stage
+    >>> is_valid_transition('sign', 'sign')
+    False
+    >>> is_valid_transition('sign', 'seal')
+    False
+    >>> is_valid_transition('sign', 'send')
+    False
+    >>> is_valid_transition('seal', 'send')
+    True
+    >>> is_valid_transition('send', 'sign')
+    True
+    >>> is_valid_transition('seal', 'seal')
+    True
+    >>> is_valid_transition('seal', 'seal')
+    True
+    >>> is_valid_transition('send', 'send')
+    False
+    >>> is_valid_transition('sign', 'sign')
+    False
+    >>> is_valid_transition('seal', 'sign')
+    False
+    >>> is_valid_transition('seal', 'send')
+    True
+    >>> is_valid_transition('send', 'seal')
+    True
+    >>> is_valid_transition('send', 'sign')
+    True
+
+    >>> is_valid_transition(None, 'seal')
+    True
+    >>> is_valid_transition(None, 'send')
+    Traceback (most recent call last):
+        ...
+    ValueError: Can only transition a final to the sealed stage but got target=send
+    >>> is_valid_transition(None, 'sign')
+    Traceback (most recent call last):
+        ...
+    ValueError: Can only transition a final to the sealed stage but got target=sign
+    >>> is_valid_transition('bad', 'seal')
+    Traceback (most recent call last):
+        ...
+    ValueError: sign, seal, and send are the only valid stage options. Got start=bad, target=seal
+    """
+    if start is None:
+        if target == 'seal':
+            return True
+        raise ValueError('Can only transition a final to the sealed stage but got target=%s' % target)
+    if (
+            start not in ['sign', 'seal', 'send']
+            or target not in ['sign', 'seal', 'send']
+    ):
+        raise ValueError('sign, seal, and send are the only valid stage options. Got start=%s, target=%s' % (
+            start, target
+        ))
+    return not any([
+        start == 'sign',  # sign is the last stage; can't transition from it
+        start == 'send' and target == 'send',
+        start == 'seal' and target == 'sign',
+    ])
 
 
 def copytree(src: str, dst_parent: str, dst: str) -> str:
