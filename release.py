2853a170598f2f68c3f1efa7bd49f748ce269133
Alex Sheluchin <alex@coinaccord.io>
2019-03-21 14:02:27 -0400

    fixup! WIP First pass at making `!sign` work

--- release.py
+++ release.py
@@ -340,7 +340,7 @@ class Release(BotPlugin):  # pylint:disable=too-many-ancestors
                 new_version_name = helpers.change_sha(new_version_name, git.get_ref()[:7])
                 self.jira.change_version_name(jira_version, new_version_name)
                 git.create_tag(tag_name=new_version_name)
-                git.create_ref(new_version_name=new_version_name)  # FIXME: reference already exists?
+                git.create_ref(new_version_name=new_version_name)
                 git.create_release(release_notes=release_notes, new_version_name=new_version_name)
                 if not is_hotfix:
                     git.update_develop()
